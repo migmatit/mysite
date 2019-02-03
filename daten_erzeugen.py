edd1267a40433560b970222a8150f8dda7cceef6
bernd <azurit100-1@yahoo.de>
2019-02-03 14:39:11 +0100

    Zweiter commit

--- daten_erzeugen.py
+++ daten_erzeugen.py
@@ -1,4 +1,5 @@
-import os
+import os, sys
+import traceback
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 
@@ -6,22 +7,26 @@ import django
 django.setup()
 
 from marcador.models import Vokabel
-
-pfad_zu_vokabeln = \
-		"C:/Users/Bernd Holter/Documents/python/projekte/uebungen/vokabeltrainer/vokabeln/"
-anzahl_kapitel = 0
-for kapitel in os.listdir(pfad_zu_vokabeln):
-	anzahl_kapitel = anzahl_kapitel + 1
-	pfad_zum_kapitel = pfad_zu_vokabeln + "norsk_kap_" + str(anzahl_kapitel) + ".txt"
-	vokabeln = open(pfad_zum_kapitel)
-	for line in vokabeln:
-		line = line.strip(" ")
-		line = line.rstrip()
-		aufteilen = line.split(":")
-		norw = aufteilen[0]
-		deu = aufteilen[1]
-		print(aufteilen)
-		v = Vokabel(norwegisch=norw, deutsch=deu, kapitel=anzahl_kapitel)
-		v.save()
-		
+try:
+	pfad_zu_vokabeln = os.path.abspath(".")
+	anzahl_kapitel = 0
+	for kapitel in os.listdir(pfad_zu_vokabeln):
+		anzahl_kapitel = anzahl_kapitel + 1
+		pfad_zum_kapitel = pfad_zu_vokabeln + os.path.sep + "Kapitel" + os.path.sep +"norsk_kap_" + str(anzahl_kapitel) + ".txt"
+		vokabeln = open(pfad_zum_kapitel)
+		for line in vokabeln:
+			line = line.strip(" ")
+			line = line.rstrip()
+			aufteilen = line.split(":")
+			norw = aufteilen[0]
+			deu = aufteilen[1]
+			print(aufteilen)
+			v = Vokabel(norwegisch=norw, deutsch=deu, kapitel=anzahl_kapitel)
+			v.save()
 	vokabeln.close()
+	input()
+except Exception as e:
+	print(traceback.format_exception(None, # <- type(e) by docs, but ignored 
+                                     e, e.__traceback__),
+          file=sys.stderr, flush=True)
+	input()
--- daten_holen.py
+++ daten_holen.py
@@ -8,3 +8,5 @@ from django.db.models import Max
 from marcador.models import Vokabel
 
 vokabeln = Vokabel.objects.filter(kapitel=4)
+print(vokabeln)
+input()
